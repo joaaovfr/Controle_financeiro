df202d89793a2cdd073b5d9d48ffb1222382cf91
12345678Ab <joaaovfr@gmail.com>
2023-12-05 22:35:52 -0300

    projeto

---
+++ src/components/Header/styles.js
@@ -0,0 +1,18 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+
+    height: 150px;
+    text-align: center;
+    background-color:#228B22;
+`
+
+export const Header = styled.h1`
+
+
+`
+
+export const Title = styled.div`
+    padding-top: 10px;
+    color: #000000;
+`
